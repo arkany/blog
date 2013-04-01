41a0b4a8648645e73438ab91f88a3bc363514f97
David Phillips <davidphillips@Davids-MacBook-Pro.local>
2013-03-31 20:33:14 -0400

    partially done portfolio section, very close to ready for the web

--- _site/Gruntfile-ck.js
+++
@@ -1,1 +0,0 @@
-module.exports=function(e){e.loadNpmTasks("grunt-contrib-qunit");e.loadNpmTasks("grunt-contrib-watch");e.initConfig({qunit:{all:["test/javascripts/tests/**/*.html"]},watch:{all:{files:["test/javascripts/tests/**/*.html","test/javascripts/tests/**/*.js","lib/assets/javascripts/foundation/*.js"],tasks:"default",options:{interrupt:!0}}}});e.registerTask("default",["qunit"])};
